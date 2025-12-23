4b45d32409f883b48a04a7e56ab7bffa6739bd10
mithun <mithunjerry24@gmail.com>
2025-12-23 19:54:55 +0530

    Fix tasks router path to allow POST /api/tasks/

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, CategoryViewSet, AttachmentDownloadViewSet
 
 router = DefaultRouter()
-router.register(r"tasks", TaskViewSet, basename="task")
+router.register(r"", TaskViewSet, basename="task")          # 🔥 FIX HERE
 router.register(r"categories", CategoryViewSet, basename="category")
 router.register(r"attachments", AttachmentDownloadViewSet, basename="attachment-download")
 
